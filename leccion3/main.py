3eeea4e8c30175a02d34a0d6484ba1440da0345f
oscar ordoñez izquierdo <oordoneiz@hotmail.com>
2022-07-13 01:17:55 +0200

    Ejemplo de como utilizar flask

---
+++ leccion3/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+registro = ['07-07-2022','10:30','15:00',6.50,5,25]
+@app.route('/')
+def home():
+    context = {
+        'titulo':'Registro de Hora de trabajo',
+        'registros':registro
+    }
+    return render_template('index.html',**context)
